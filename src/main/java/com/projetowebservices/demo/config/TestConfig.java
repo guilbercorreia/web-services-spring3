d4f31673939a6a5b8197645d27213955ca96cf78
Guilber Correia <guilber.correia.gc@gmail.com>
2023-06-14 16:05:04 -0300

    criando instancias para categoria

--- src/main/java/com/projetowebservices/demo/config/TestConfig.java
+++ src/main/java/com/projetowebservices/demo/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projetowebservices.demo.entities.Category;
 import com.projetowebservices.demo.entities.Order;
 import com.projetowebservices.demo.entities.User;
 import com.projetowebservices.demo.entities.enums.OrderStatus;
+import com.projetowebservices.demo.repositories.CategoryRepository;
 import com.projetowebservices.demo.repositories.OrderRepository;
 import com.projetowebservices.demo.repositories.UserRepository;
 
@@ -24,16 +26,24 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Electronicos");
+		Category cat2 = new Category(null, "Livros");
+		Category cat3 = new Category(null, "PC");
 
 		User u1 = new User(null, "Estevão Ferreia", "estevao.f@gmail.com", "88888888", "12345678");
 		User u2 = new User(null, "Alex Marques", "alex.m@gmail.com", "77777777", "12345678");
 		Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 		Order o2 = new Order(null, Instant.parse("2023-06-21T03:42:10Z"), u2, OrderStatus.CANCELED);
-		Order o3 = new Order(null, Instant.parse("2023-06-22T15:21:22Z"), u1 , OrderStatus.DELIVERED);
+		Order o3 = new Order(null, Instant.parse("2023-06-22T15:21:22Z"), u1, OrderStatus.DELIVERED);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 }

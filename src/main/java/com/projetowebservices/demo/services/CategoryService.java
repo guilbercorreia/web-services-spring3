9cc1939330db8f0cd132f7d71c90b496e43e1f1d
Guilber Correia <guilber.correia.gc@gmail.com>
2023-06-14 16:06:15 -0300

    criação do serviço da categoria

---
+++ src/main/java/com/projetowebservices/demo/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.projetowebservices.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetowebservices.demo.entities.Category;
+import com.projetowebservices.demo.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository Repository;
+
+	public List<Category> findAll(){
+		return Repository.findAll();
+	}
+	public Category findById(long id){
+		Optional<Category> obj = Repository.findById(id);
+		return obj.get(); 
+	
+}
+}

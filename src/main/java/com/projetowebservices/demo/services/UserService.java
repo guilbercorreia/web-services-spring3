d116c8ee0686da8bef2d4cd182b502830129c822
Guilber Correia <guilber.correia.gc@gmail.com>
2023-06-15 21:28:12 -0300

    Tratamento de exceções

--- src/main/java/com/projetowebservices/demo/services/UserService.java
+++ src/main/java/com/projetowebservices/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetowebservices.demo.entities.User;
 import com.projetowebservices.demo.repositories.UserRepository;
+import com.projetowebservices.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 
 	public User findById(long id) {
 		Optional<User> obj = Repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
